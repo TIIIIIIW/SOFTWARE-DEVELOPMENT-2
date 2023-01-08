14309c0b4745e4418d156b5fcf2a2ca2d2bfdcc2
TIIIIIIW <109777077+TIIIIIIW@users.noreply.github.com>
2023-01-08 21:32:24 +0700

    Update Function

--- ML/Test.py
+++ ML/Test.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import yfinance as yf
+from datetime import datetime
 import unittest
+from unittest.mock import patch
+
+#--------------------------------------------------------------------------------------------------------#
 
 def getData(tickers) :
     data = []
@@ -25,10 +29,44 @@ class Testgetdata(unittest.TestCase):
             symbol = ticker[:-3]
             self.assertEqual(df['Symbol'].unique()[0], symbol)
 
-    def test_getData_noInput(self):
+    def test_getDataNoInput(self):
         tickers = []
         data = getData(tickers)
         self.assertEqual(data, 'No stock name')
+
+#--------------------------------------------------------------------------------------------------------#
+
+def updateData(tickers):
+    if tickers == []:
+        return 'No stock name'
+    stock_data = pd.read_csv('stock_dataTest.csv')
+    new_data = [stock_data]
+    stock_data['Datetime'] = pd.to_datetime(stock_data['Datetime'], format='%Y/%m/%d')
+    start_date = max(stock_data['Datetime'])
+    today_date = datetime.now()
+    for ticker in tickers:
+        detail = yf.download(ticker, interval='1h', start=start_date, end=today_date)
+        detail['Symbol'] = ticker[:-3]
+        table_price = detail.reset_index()
+        new_data.append(table_price)
+    new_table = pd.concat(new_data)
+    duplicate = new_table[['Datetime','Symbol']].duplicated(keep='first')
+    new_table = new_table.drop(new_table[duplicate].index)
+    new_table = new_table.sort_values(by=['Symbol', 'Datetime'], ascending=True)
+    new_table.to_csv('stock_dataTest.csv', index=False)
+
+
+class Testupdatedata(unittest.TestCase):
+    def test_updateNoInput(self):
+        result = updateData([])
+        self.assertEqual(result, 'No stock name')
+
+    # @patch('stock_dataTest.csv')
+    # @patch('yf.download')
+    # def test_updateData(self, mock_yf_download, mock_csv):
+
+
+#--------------------------------------------------------------------------------------------------------#
 
 if __name__ == '__main__':
     unittest.main()
